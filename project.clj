158d20d228bfd8d88cfecc89ef4a153005abfcb5
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-07-09 08:53:56 -0700

    Moved over to lein-iplant-rpm.

--- project.clj
+++ project.clj
@@ -6,5 +6,10 @@
                  [org.iplantc/clojure-commons "1.2.0-SNAPSHOT"]
                  [clj-time "0.3.7"]
                  [slingshot "0.10.1"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.3.0-SNAPSHOT"]]
+  :iplant-rpm {:summary "panopticon"
+               :dependencies ["iplant-service-config >= 0.1.0-5"]
+               :config-files ["log4j.properties"]
+               :config-path "conf"}
   :aot [panopticon.core]
   :main panopticon.core)

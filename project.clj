afaa0ca95c3c1d0552342b77cfab87e307b7c45e
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-07-26 15:04:30 -0700

    Moving to new lein-iplant-rpm.

--- project.clj
+++ project.clj
@@ -6,8 +6,9 @@
                  [org.iplantc/clojure-commons "1.2.0-SNAPSHOT"]
                  [clj-time "0.3.7"]
                  [slingshot "0.10.1"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.3.0-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.3.1-SNAPSHOT"]]
   :iplant-rpm {:summary "panopticon"
+               :runuser "condor"
                :dependencies ["iplant-service-config >= 0.1.0-5"]
                :config-files ["log4j.properties"]
                :config-path "conf"}

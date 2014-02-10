a99a66f272990a32f17eab748efb6938e8f6ce5d
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-10 15:10:25 -0700

    CORE-5133: removed all snapshot dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/panopticon "1.0.2-SNAPSHOT"
+(defproject org.iplantc/panopticon "1.0.2"
   :description "A background service for monitoring the statuses of Condor jobs."
   :url "http://www.iplantcollaborative.org"
   :license {:name "BSD"
@@ -11,11 +11,11 @@
                   [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.iplantc/clojure-commons "1.4.8-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.8"]
                  [cheshire "5.0.1"]
                  [clj-time "0.4.4"]
                  [slingshot "0.10.3"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.4.3-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.4.3"]]
   :iplant-rpm {:summary "panopticon"
                :runuser "condor"
                :dependencies ["iplant-service-config >= 0.1.0-5"
